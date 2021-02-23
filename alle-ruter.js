c4cefdf9d0374219573f8f35fa0c2a94514aa893
Victor Seifert Hegelund <Victor@Victors-MacBook-Pro.local>
2021-02-23 10:59:14 +0100

    top-tekst.html

--- alle-ruter.js
+++ alle-ruter.js
@@ -1,50 +1,57 @@
+const template = document.querySelector("template");
+
+const url = "https://titusinde-2763.restdb.io/rest/ruter";
+const imgUrl = "https://titusinde-2763.restdb.io/media/";
+
+let ruter;
+
+let filterMin = 0;
+let filterMax = 10000;
+
+const options = {
+    headers: {
+        'x-apikey': "602e73575ad3610fb5bb6331"
+    }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-    const template = document.querySelector("template");
+    hentdata();
+    //    setLeftValue();
+    //    setRightValue();
+});
 
-    const url = "https://titusinde-2763.restdb.io/rest/ruter";
-    const imgUrl = "https://titusinde-2763.restdb.io/media/";
 
-    let filterMin;
-    let filterMax;
+async function hentdata() {
+    console.log("hentdata")
+    const result = await fetch(url, options);
+    ruter = await result.json();
+    vis();
+}
 
-    const options = {
-        headers: {
-            'x-apikey': "602e73575ad3610fb5bb6331"
+function vis() {
+    document.querySelector(".rute-list .content").textContent = "";
+    console.log("vis");
+    console.log(ruter);
+    console.log("filterMin: " + filterMin);
+    console.log("filterMax: " + filterMax);
+
+    ruter.forEach(rute => {
+        if (rute.skridt >= filterMin && rute.skridt <= filterMax) {
+            let klon = template.cloneNode(true).content;
+            klon.querySelector(".the_img").src = imgUrl + rute.billede;
+            klon.querySelector("h2").textContent = rute.navn
+            klon.querySelector(".skridt").textContent = `${rute.skridt} skridt`;
+            klon.querySelector(".placering").textContent = rute.placering;
+            klon.querySelector(".the_img").addEventListener("click", () => visDetaljer(rute))
+            klon.querySelector("button").addEventListener("click", () => visDetaljer(rute))
+            document.querySelector(".rute-list .content").appendChild(klon);
         }
-    };
+    })
+}
 
-    hentdata(url);
-    async function hentdata() {
-        console.log("hentdata")
-        const result = await fetch(url, options);
-        const json = await result.json();
-        console.log(json)
-        vis(json);
-    }
-
-    function vis(ruter) {
-        document.querySelector(".rute-list .content").textContent = "";
-        console.log("vis");
-        ruter.forEach(rute => {
-            //            console.log("filterMin: " + filterMin)
-            //            console.log("filterMax: " + filterMax)
-            //            console.log("rute.skridt: " + rute.skridt)
-            if (rute.skridt >= filterMin && rute.skridt <= filterMax) {
-                let klon = template.cloneNode(true).content;
-                klon.querySelector(".the_img").src = imgUrl + rute.billede;
-                klon.querySelector("h2").textContent = rute.navn
-                klon.querySelector(".skridt").textContent = `${rute.skridt} skridt`;
-                klon.querySelector(".placering").textContent = rute.placering;
-                klon.querySelector(".the_img").addEventListener("click", () => visDetaljer(rute))
-                klon.querySelector("button").addEventListener("click", () => visDetaljer(rute))
-                document.querySelector(".rute-list .content").appendChild(klon);
-            }
-        })
-    }
-
-    function visDetaljer(rute) {
-        location.href = `rute.html?id=${rute._id}`;
-    }
+function visDetaljer(rute) {
+    location.href = `rute.html?id=${rute._id}`;
+}
 
 
 
@@ -54,89 +61,86 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-    var inputLeft = document.getElementById("input-left");
-    var inputRight = document.getElementById("input-right");
+var inputLeft = document.getElementById("input-left");
+var inputRight = document.getElementById("input-right");
 
-    var thumbLeft = document.querySelector(".slider .thumb.left");
-    var thumbRight = document.querySelector(".slider .thumb.right");
-    var range = document.querySelector(".slider .range");
+var thumbLeft = document.querySelector(".slider .thumb.left");
+var thumbRight = document.querySelector(".slider .thumb.right");
+var range = document.querySelector(".slider .range");
 
-    function setLeftValue() {
-        var _this = inputLeft,
-            min = parseInt(_this.min),
-            max = parseInt(_this.max);
+function setLeftValue() {
+    var _this = inputLeft,
+        min = parseInt(_this.min),
+        max = parseInt(_this.max);
 
-        _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
+    _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
 
-        document.querySelector("#output-left").textContent = _this.value;
+    document.querySelector("#output-left").textContent = _this.value;
 
-        var percent = ((_this.value - min) / (max - min)) * 100;
+    var percent = ((_this.value - min) / (max - min)) * 100;
 
-        thumbLeft.style.left = percent + "%";
-        range.style.left = percent + "%";
+    thumbLeft.style.left = percent + "%";
+    range.style.left = percent + "%";
 
-        filterRuterMin(_this);
-    }
-    setLeftValue();
-
-    function setRightValue() {
-        var _this = inputRight,
-            min = parseInt(_this.min),
-            max = parseInt(_this.max);
-
-        _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
-
-        document.querySelector("#output-right").textContent = _this.value;
-
-        var percent = ((_this.value - min) / (max - min)) * 100;
-
-        thumbRight.style.right = (100 - percent) + "%";
-        range.style.right = (100 - percent) + "%";
-
-        filterRuterMax(_this);
-    }
-    setRightValue();
-
-    inputLeft.addEventListener("input", setLeftValue);
-    inputRight.addEventListener("input", setRightValue);
-
-    inputLeft.addEventListener("mouseover", function () {
-        thumbLeft.classList.add("hover");
-    });
-    inputLeft.addEventListener("mouseout", function () {
-        thumbLeft.classList.remove("hover");
-    });
-    inputLeft.addEventListener("mousedown", function () {
-        thumbLeft.classList.add("active");
-    });
-    inputLeft.addEventListener("mouseup", function () {
-        thumbLeft.classList.remove("active");
-    });
-
-    inputRight.addEventListener("mouseover", function () {
-        thumbRight.classList.add("hover");
-    });
-    inputRight.addEventListener("mouseout", function () {
-        thumbRight.classList.remove("hover");
-    });
-    inputRight.addEventListener("mousedown", function () {
-        thumbRight.classList.add("active");
-    });
-    inputRight.addEventListener("mouseup", function () {
-        thumbRight.classList.remove("active");
-    });
-
-    function filterRuterMin(_this) {
-        filterMin = _this.value;
-        console.log("Min: " + filterMin);
-        hentdata();
-    }
-
-    function filterRuterMax(_this) {
-        filterMax = _this.value;
-        console.log("Max: " + filterMax);
-        hentdata();
-    }
+    filterRuterMin(_this);
+}
 
 
-})
+function setRightValue() {
+    var _this = inputRight,
+        min = parseInt(_this.min),
+        max = parseInt(_this.max);
+
+    _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
+
+    document.querySelector("#output-right").textContent = _this.value;
+
+    var percent = ((_this.value - min) / (max - min)) * 100;
+
+    thumbRight.style.right = (100 - percent) + "%";
+    range.style.right = (100 - percent) + "%";
+
+    filterRuterMax(_this);
+}
+
+
+inputLeft.addEventListener("input", setLeftValue);
+inputRight.addEventListener("input", setRightValue);
+
+inputLeft.addEventListener("mouseover", function () {
+    thumbLeft.classList.add("hover");
+});
+inputLeft.addEventListener("mouseout", function () {
+    thumbLeft.classList.remove("hover");
+});
+inputLeft.addEventListener("mousedown", function () {
+    thumbLeft.classList.add("active");
+});
+inputLeft.addEventListener("mouseup", function () {
+    thumbLeft.classList.remove("active");
+});
+
+inputRight.addEventListener("mouseover", function () {
+    thumbRight.classList.add("hover");
+});
+inputRight.addEventListener("mouseout", function () {
+    thumbRight.classList.remove("hover");
+});
+inputRight.addEventListener("mousedown", function () {
+    thumbRight.classList.add("active");
+});
+inputRight.addEventListener("mouseup", function () {
+    thumbRight.classList.remove("active");
+});
+
+function filterRuterMin(_this) {
+    filterMin = _this.value;
+    console.log("Min: " + filterMin);
+    vis();
+}
+
+function filterRuterMax(_this) {
+    filterMax = _this.value;
+    console.log("Max: " + filterMax);
+    vis();
+}
